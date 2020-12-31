e00afb12ef59e3a838997f82b133d4c0970505a6
Adam Green <adam.green@adgefficiency.com>
2021-01-01 07:41:39 +1300

    aljazeera working

--- climatedb/newspapers/aljazeera.py
+++ climatedb/newspapers/aljazeera.py
@@ -1,21 +1,9 @@
 from datetime import datetime
-import json
-import logging
+import re
 
-from bs4 import BeautifulSoup
 import requests
 
-from climatedb.utils import find_one_tag, form_article_id
-
-
-
-def application_json_helper(soup):
-    app = soup.findAll("script", {"type": "application/ld+json"})
-    for a in app:
-        try:
-            print(a["type"])
-        except KeyError:
-            pass
+from climatedb import utils
 
 
 def strip_aljazzera_dt(date_str):
@@ -23,12 +11,12 @@ def strip_aljazzera_dt(date_str):
         date = datetime.strptime(date_str, "%d %b %Y %H:%M GMT")
     except ValueError:
         date = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S%z")
-    return date.isoformat() 
+    return date.isoformat()
 
 
-def check_aljazzera_url(url, logger=None):
+def check_url(url):
+    return url
     res = requests.get(url, allow_redirects=True)
-    logger.info(f"newspaper=aljazeera, url={url}, redirect={res.url}")
     url = res.url
 
     if (
@@ -45,26 +33,30 @@ def check_aljazzera_url(url, logger=None):
 
     if "/tag/" in url:
         return False
-
     if url == "https://www.aljazeera.com/":
         return False
     if url == "https://www.aljazeera.com/news/":
         return False
     if url == "https://www.aljazeera.com/tag/climate-change/":
         return False
-    return True
+
+    return url
 
 
-def parse_aljazzera_url(url, logger=None):
-    logger = logging.getLogger("climatedb")
-    response = requests.get(url, allow_redirects=True)
-    html = response.text
-    soup = BeautifulSoup(html, features="html5lib")
+def get_article_id(url):
+    return utils.form_article_id(url, -1)
+
+
+def parse_url(url):
+    response = utils.request(url)
+    soup = response['soup']
+    html = response['html']
 
     try:
-        body = find_one_tag(soup, "div", {"class": "main-article-body"})
+        body = utils.find_one_tag(soup, "div", {"class": "main-article-body"})
         body = body.findAll("p")
-    except AssertionError:
+
+    except utils.ParserError:
         #  possible to have multiple 'text section' divs
         body = soup.findAll("div", {"class": "text section"})
         p_tags = []
@@ -73,30 +65,23 @@ def parse_aljazzera_url(url, logger=None):
         body = p_tags
 
     if len(body) == 0:
-        body = find_one_tag(soup, "div", {"class": "wysiwyg wysiwyg--all-content"})
+        body = utils.find_one_tag(soup, "div", {"class": "wysiwyg wysiwyg--all-content"})
         body = body.findAll("p")
 
-    body = "".join(p.text for p in body)
+    body = "".join([p.text for p in body])
 
-    #  hoping it is the firstone
-    #  there are usually 2/3 ld/json
-    app = soup.findAll("script", {"type": "application/ld+json"})
-    if len(app) == 0:
-        return {"error": "no application/ld+json"}
-
-    assert len(app) >= 1
-    app = app[0].text.replace("\n", "")
-    app = json.loads(app)
+    app = utils.find_application_json(soup, 'headline')
+    headline = app['headline']
+    published = app['datePublished']
 
     return {
-        "newspaper_id": "aljazeera",
+        **aljazeera,
         "body": body,
-        "article_id": form_article_id(url),
-        "headline": app["headline"],
+        "headline": headline,
         "article_url": url,
         "html": html,
+        "article_id": get_article_id(url),
         "date_published": strip_aljazzera_dt(app["datePublished"]),
-        "date_modified": strip_aljazzera_dt(app["dateModified"]),
     }
 
 
@@ -104,12 +89,9 @@ aljazeera = {
     "newspaper_id": "aljazeera",
     "newspaper": "Al Jazeera",
     "newspaper_url": "aljazeera.com",
-    "checker": check_aljazzera_url,
-    "parser": parse_aljazzera_url,
+
+    "checker": check_url,
+    "parser": parse_url,
+    "get_article_id": get_article_id,
     "color": "#FA9000",
 }
-if __name__ == "__main__":
-    url = "http://www.aljazeera.com/news/europe/2007/02/2008525144121633730.html"
-    response = requests.get(url, allow_redirects=False)
-
-    par = parse_aljazzera_url(url)
--- climatedb/registry.py
+++ climatedb/registry.py
@@ -1,20 +1,20 @@
 import random
 
+from climatedb.newspapers.bbc import bbc
 from climatedb.newspapers.cnn import cnn
+from climatedb.newspapers.economist import economist
 from climatedb.newspapers.guardian import guardian
 from climatedb.newspapers.fox import fox
+from climatedb.newspapers.nzherald import nzherald
 from climatedb.newspapers.skyau import skyau
 from climatedb.newspapers.nytimes import nytimes
 
-from climatedb.newspapers.economist import economist
 from climatedb.newspapers.newshub import newshub
-from climatedb.newspapers.nzherald import nzherald
 from climatedb.newspapers.stuff import stuff
 from climatedb.newspapers.aljazeera import aljazeera
 from climatedb.newspapers.atlantic import atlantic
 from climatedb.newspapers.washington_post import washington_post
 from climatedb.newspapers.dw import dw
-from climatedb.newspapers.bbc import bbc
 from climatedb.newspapers.independent import independent
 from climatedb.newspapers.dailymail import dailymail
 
@@ -23,26 +23,6 @@ def find_newspaper_from_url(url):
     for paper in registry:
         if paper["newspaper_url"] in url:
             return paper
-
-
-registry = [
-    guardian,
-    bbc,
-    nzherald,
-    stuff,
-    newshub,
-    economist,
-    aljazeera,
-    atlantic,
-    washington_post,
-    cnn,
-    dw,
-    independent,
-    dailymail,
-    fox,
-    nytimes,
-    skyau,
-]
 
 
 def get_newspaper(newspaper):
@@ -61,3 +41,23 @@ def get_newspapers_from_registry(newspapers=None):
         papers = [n for n in registry if n["newspaper_id"] in newspapers]
     random.shuffle(papers)
     return papers
+
+
+registry = [
+    guardian,
+    bbc,
+    nzherald,
+    stuff,
+    newshub,
+    economist,
+    aljazeera,
+    atlantic,
+    washington_post,
+    cnn,
+    dw,
+    independent,
+    dailymail,
+    fox,
+    nytimes,
+    skyau
+]
