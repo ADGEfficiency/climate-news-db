a963789623cd9accad22db95e86d78ded4432bdb
Adam Green <adam.green@adgefficiency.com>
2022-04-23 04:26:05 +1200

    rm collect

--- climatedb/lambda/handlers.py
+++ climatedb/lambda/handlers.py
@@ -1,32 +1,19 @@
-import asyncio
-import json
-
-import boto3
-from rich import print
-
-from climatedb.search import search
-from climatedb import types
-
-from climatedb.databases import find_all_papers
+from climatedb import types, files
 from climatedb.config import data_home, s3_bucket, s3_prefix
-
-from climatedb import files
+from climatedb.databases import find_all_papers
+from climatedb.search import search
 
 
 def controller_handler(
     event: dict, context: types.Union[dict, None] = None
 ) -> types.Dict[str, str]:
-    """
-    Could use the search lambda here, or just the handler here
-    """
+    """Could use the search lambda here, or just the handler here"""
 
     bucket = event.get("s3_bucket", s3_bucket)
     key = event.get("s3_key", s3_prefix + "/urls.jsonl")
-
     urls_db = files.S3JSONLines(event["s3_bucket"], event["s3_key"])
 
     #  TODO - create a daily stamp file as simple backup (maybe)
-
     pkg = []
     for np in find_all_papers():
         urls = search(np, 5)
