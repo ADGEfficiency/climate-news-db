5e9e2a1a339b3b7fd5a2bcbde5efa1a997b8ac67
Adam Green <adam.green@adgefficiency.com>
2022-10-08 16:34:53 +1300

    script cleanup

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,6 +1,9 @@
 echo "url counts"
 wc -l data-neu/urls.jsonl
 wc -l data-neu/urls.csv
+wc -l data-neu/rejected.jsonlines
 
-echo "article json files"
+echo "\narticle json files"
 find data-neu/articles/ -name '*.jsonlines' | xargs -n 1 -I {} wc -l {}
+
+python ./inspect_db.py
