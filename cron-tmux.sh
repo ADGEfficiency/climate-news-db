d6a583a419797104d985ab8aaa471a153cd24d2f
Adam Green <adam.green@adgefficiency.com>
2023-01-18 03:21:44 +1300

    [BUG Prune docker images in cron-tmux (#27)

--- cron-tmux.sh
+++ cron-tmux.sh
@@ -2,4 +2,5 @@
 . /home/ubuntu/.profile
 . /home/ubuntu/.bashrc
 cd /home/ubuntu/climate-news-db && /usr/bin/tmux new -d -s cron-scraper
-/usr/bin/tmux send-keys -t cron-scraper "/usr/bin/make scrape; sudo shutdown" C-m
+docker system prune --all --force
+/usr/bin/tmux send-keys -t cron-scraper "/usr/bin/make scrape >> ~/cron-log.txt && sudo shutdown" C-m
